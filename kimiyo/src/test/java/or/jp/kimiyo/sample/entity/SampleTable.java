124592a5238edc5939b10f8ed263cb66b8927b11
o-lee <o-lee@fores.jp>
2016-03-15 12:11:53 +0900

    sample Entity

---
+++ kimiyo/src/test/java/or/jp/kimiyo/sample/entity/SampleTable.java
@@ -0,0 +1,63 @@
+package or.jp.kimiyo.sample.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class SampleTable implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	private String id;
+
+	@Column(name = "name")
+	private String name;
+
+	public SampleTable() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+		if (!(object instanceof SampleTable)) {
+			return false;
+		}
+		SampleTable other = (SampleTable) object;
+		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "or.jp.kimiyo.entity.SampleTable[ id=" + id + " ]";
+	}
+}
