cfa2bdbb9e9b4389c524182c1f39394f3b6608c0
o-lee <o-lee@fores.jp>
2016-03-15 12:32:55 +0900

    Maven設定修正、Sample Repository追加

---
+++ kimiyo/src/test/java/or/jp/kimiyo/sample/repository/SampleTableRepository.java
@@ -0,0 +1,14 @@
+package or.jp.kimiyo.sample.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import or.jp.kimiyo.sample.entity.SampleTable;
+
+@Repository
+public interface SampleTableRepository extends JpaRepository<SampleTable, Integer> {
+
+	List<SampleTable> findAll();
+}
