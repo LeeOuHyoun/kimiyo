3df5c99046be9ee4b388c7df1b4089547eea7455
o-lee <o-lee@fores.jp>
2016-03-15 10:14:16 +0900

    test commit

--- kimiyo/src/test/java/or/jp/kimiyo/sample/controller/DbSampleController.java
+++ kimiyo/src/test/java/or/jp/kimiyo/sample/controller/DbSampleController.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * DB Access Sample Controller Class
- * 
+ *
  * @author leeouhyoun
  *
  */
 @Controller
 public class DbSampleController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(DbSampleController.class);
-	
+
 	@RequestMapping(value = "/sample/dbaccess", method = RequestMethod.GET)
 	public String sampleDbAccess(Model model) {
 		logger.info("DB Access Sample start..!");
-		
+
 		logger.info("DB Access Sample end..!");
 		return "/sample/sample_db_get";
 	}
-	
+
 }
