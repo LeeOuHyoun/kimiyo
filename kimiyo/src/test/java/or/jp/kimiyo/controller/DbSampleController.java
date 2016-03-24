fae97626a7293d0862bff0f023e48363405fc874
o-lee <o-lee@fores.jp>
2016-03-24 12:13:47 +0900

    JPA 設定中

--- kimiyo/src/test/java/or/jp/kimiyo/controller/DbSampleController.java
+++ kimiyo/src/test/java/or/jp/kimiyo/controller/DbSampleController.java
@@ -2,20 +2,17 @@ package or.jp.kimiyo.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.inject.Inject;
 
-import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import or.jp.kimiyo.entity.SampleTable;
-import or.jp.kimiyo.service.DbSampleService;
+import or.jp.kimiyo.repository.SampleTableRepository;
 
 /**
  * DB Access Sample Controller Class
@@ -28,24 +25,15 @@ public class DbSampleController {
 
 	private static final Logger logger = LoggerFactory.getLogger(DbSampleController.class);
 
-	@SuppressWarnings("null")
+	@Inject
+	private SampleTableRepository sampleTableRepository;
+
 	@RequestMapping(value = "/sample/dbaccess", method = RequestMethod.GET)
 	public String sampleDbAccess(Model model) {
 		logger.info("DB Access Sample start..!");
-		EntityManager emn = null;
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		EntityManagerFactory emf = (EntityManagerFactory) context.getBean("entityManagerFactory");
-		emn = emf.createEntityManager();
-		Session session = emn.unwrap(Session.class);
-		DbSampleService dbSample = context.getBean(DbSampleService.class);
-		List<SampleTable> resultList = dbSample.getDbSampleTable();
 
-//		  EntityManagerFactory emf = context.getBean(EntityManagerFactory.class);
-//        EntityManager em = emf.createEntityManager();
-//        em.getTransaction().begin();
-//        Query query = (Query) em.unwrap(Session.class).getNamedQuery("finishPosContract");
-//        List<Integer> rec = query.list();
-//        em.getTransaction().commit();
+		List<SampleTable> resultList = this.sampleTableRepository.findAll();
+
 		logger.info("DB Access Sample end..!");
 		return "/sample/sample_db_get";
 	}
