9061357c9f07f4a0409e8ca7c94f2d84c4679212
o-lee <o-lee@fores.jp>
2016-03-28 17:42:00 +0900

    いろいろ設定変更

--- kimiyo/src/test/java/or/jp/kimiyo/entity/SampleTable.java
+++ kimiyo/src/test/java/or/jp/kimiyo/entity/SampleTable.java
@@ -40,29 +40,4 @@ public class SampleTable implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? name.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-		if (!(object instanceof SampleTable)) {
-			return false;
-		}
-		SampleTable other = (SampleTable) object;
-		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "or.jp.kimiyo.entity.SampleTable[ id=" + id + " ]";
-	}
 }
