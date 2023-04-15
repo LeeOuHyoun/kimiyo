38bfc7bccab221bd7f35d86ac0ad3ae720c0a8e2
LeeOuHyoun <leeouhyoun@gmail.com>
2023-04-15 10:03:03 +0900

    Revert "test commit"
    This reverts commit b7cb0ff6d49b120aff1009bc402a761e805bd214.

--- kimiyo/src/main/java/or/jp/kimiyo/service/DbSampleService.java
+++ kimiyo/src/main/java/or/jp/kimiyo/service/DbSampleService.java
@@ -13,7 +13,7 @@ import or.jp.kimiyo.entity.SampleTable;
 import or.jp.kimiyo.repository.SampleTableRepository;
 
 /**
- * 「sample類table」テーブルからデータ取得 test
+ * 「sample類table」テーブルからデータ取得
  *
  * @author olee
  *
